d90521804ecf7dbca2dc0af6f8b5e03e30eb6df9
1210xx <jie_ren_rj@163.com>
2022-02-23 15:25:14 +0800

    编辑注释

--- src/webtest/ServerTest.java
+++ src/webtest/ServerTest.java
@@ -13,35 +13,43 @@ import java.nio.charset.StandardCharsets;
  */
 public class ServerTest {
     public static void main(String[] args) throws IOException {
-        //创建并监听端口
+        //创建并绑定端口
         ServerSocket socket = new ServerSocket(8080);
         System.out.println("Server is running....(Just listening)");
         //开启线程，处理接口数据
         for (;;) {
-            //
+            //监听于此端口连接的socket并且接收发送信息。
             Socket socket_l = socket.accept();
             //输出接收到的接口地址
             System.out.println("Connected from " + socket_l.getRemoteSocketAddress());
             //启动线程处理接口接收到信息
             Thread handleThread = new SocketHandle(socket_l);
+            //启动线程
             handleThread.run();
         }
     }
 }
 
+//socket 处理类
 class SocketHandle extends Thread {
+    //socket 字段
     Socket socket;
-
+    //socketHandle 无参构造方法
     SocketHandle(Socket socket) {
         this.socket = socket;
     }
 
+    // 线程必须重写run方法实现业务
     @Override
     public void run() {
+        //父类的run方法
         super.run();
         try {
+            //获取socket的输入流，有异常抛出
             InputStream input = this.socket.getInputStream();
+            //如果输出无异常
             try (OutputStream output = this.socket.getOutputStream()) {
+                //通过输入流（请求）和处理得到输出流
                 handle(input, output);
             }
         } catch (Exception e) {
