a5080da744b5194fc781b32f1a869075440a2695
HotG <hotttj@163.com>
2022-03-16 22:04:52 +0800

    新增logback依赖和配置

--- src/main/java/trysome/springweb/SpringMVCConfig.java
+++ src/main/java/trysome/springweb/SpringMVCConfig.java
@@ -1,5 +1,10 @@
 package trysome.springweb;
 
+/**
+ * src/main/webapp是标准web目录，
+ * WEB-INF存放web.xml，编译的class，第三方jar，以及不允许浏览器直接访问的View模版，
+ * static目录存放所有静态文件。
+ */
 public class SpringMVCConfig {
 
 }
